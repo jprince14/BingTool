21fe70091f0688bfbe0f764ff1946a66e01cf006
jprince14 <jprinc10@jhu.edu>
2016-06-05 23:00:26 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,35 @@
-# def seleniumCheck():
-#     import pip
-#     #Make sure that Selenium is installed
-#     installed_packages = pip.get_installed_distributions()
-#     flat_installed_packages = [package.project_name for package in installed_packages]
-#     if "selenium" not in flat_installed_packages:
-#         pip.main(['install', '-U', 'selenium'])
-    
-
 from FirefoxWebDriver import FirefoxWebDriver
 from ChromeWebDriver import ChromeWebDriver
 from Searches import Searches
 import random
 from time import sleep
-
+import sys
 
 Edge = "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.71 Safari/537.36 Edge/12.0"
 SafariMobile = "Mozilla/5.0 (iPhone; CPU iPhone OS 9_2 like Mac OS X) AppleWebKit/601.1 (KHTML, like Gecko) CriOS/47.0.2526.70 Mobile/13C71 Safari/601.1.46"
 
 base_url = "http://www.bing.com/search?q="
 
-
-#searches, http://www.google.com/trends/hottrends/atom/feed
-
+def checkDependencies():
+    import pip
+    #Make sure that Selenium is installed
+    installed_packages = pip.get_installed_distributions()
+    flat_installed_packages = [package.project_name for package in installed_packages]
+    if "selenium" not in flat_installed_packages:
+        try:
+            pip.main(['install', '-U', 'selenium'])
+        except:
+            sys.stderr.write("ERROR: Need to install selenium")
+            
+    if "feedparser" not in flat_installed_packages:
+        try:
+            pip.main(['install', '-U', 'feedparser'])
+        except:
+            sys.stderr.write("ERROR: Need to install feedparser")
+            
 if __name__ == '__main__':
+    
+    checkDependencies()
     
     DesktopSearches = 35
     MoileSearches = 25
@@ -47,7 +54,7 @@ if __name__ == '__main__':
     firefox.startMobileDriver()
     chrome.startMobileDriver()
     
-    for index in (random.sample(range(len(searchesList)), DesktopSearches)):
+    for index in (random.sample(range(len(searchesList)), MoileSearches)):
     
         firefox.getMobileUrl(base_url + searchesList[index])
         chrome.getMobileUrl(base_url + searchesList[index])
